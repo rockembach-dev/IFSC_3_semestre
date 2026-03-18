873af67654045b22397327ba090615eaba14e34e
Gustavo Rockembach <rockembach.dev@gmail.com>
2026-03-18 10:46:53 -0300

    Comecei a fazero botao de remover o item da lista

--- PDM/revisaoJs/listaExercicios/exerc10/ex10.js
+++ PDM/revisaoJs/listaExercicios/exerc10/ex10.js
@@ -1,3 +1,5 @@
+let tarefas = [];
+
 //Arrow function
 document.getElementById("btAdd").addEventListener("click",_=>{
  //recebe o valor do input
@@ -11,20 +13,42 @@ document.getElementById("btAdd").addEventListener("click",_=>{
  //mensagem de sucesso
  }else{
   let mensagem = "Tarefa adicionada com sucesso!"
- document.getElementById("mensagem").textContent = mensagem
-
- //se a caixa nao estiver vazia, faz a lista
- let listaTarefas = document.getElementById("lista");
- let novaTarefa = document.createElement("li");
-
- novaTarefa.textContent = tarefa;
-
- //pega o elemento "pai" e adiciona um novo elemento "filho"
- listaTarefas.appendChild(novaTarefa);
-
- inputTarefa.value=""
+  mensagem.textContent = mensagem
+  tarefas.push(tarefa)
+  mostrarTarefas()
  }
+
+
+function mostrarTarefas(){
+ let listaTarefas = document.getElementById("lista");  //se a caixa nao estiver vazia, faz a lista
+ listaTarefas.innerHTML = ""
+
+ let i = 0
+ for(i; i < tarefas.length; i++){
+ let novaTarefa = document.createElement("li");
+ novaTarefa.textContent = tarefas[i];
+
+ //Botão que remove o item da lista 
+ let botaoRemover = document.createElement("button")
+ botaoRemover.textContent = "REMOVER"
+ novaTarefa.appendChild(botaoRemover)
+
+
+ listaTarefas.appendChild(novaTarefa); //pega o elemento "pai" e adiciona um novo elemento "filho"
+ }
+}
+ 
 })
+
+
+
+
+
+
+
+
+
+
 
 //função para se o usuário apertar ENTER ele adiciona.
 //O "e" é um objeto criado automaticamente pelo navegador e ele contém varias propriedades prontas com info dos eventos.
